48721f4e6149cf929265534ea33b8e54037a703b
Michael Golovanov <mike.golovanov@gmail.com>
2024-04-16 16:16:28 +0300

    extract copyData tests to NaiveRsmCopyTest

--- naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/NaiveRemoteStorageManagerTest.java
+++ naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/NaiveRemoteStorageManagerTest.java
@@ -18,16 +18,12 @@ package ru.mg.kafka.tieredstorage.minio;
 
 import java.io.InputStream;
 
-import java.nio.ByteBuffer;
-import java.nio.file.Path;
-
 import java.util.Map;
 import java.util.Optional;
 
 import org.apache.kafka.common.TopicIdPartition;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.Uuid;
-import org.apache.kafka.server.log.remote.storage.LogSegmentData;
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentId;
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentMetadata;
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentState;
@@ -37,17 +33,14 @@ import org.apache.kafka.server.log.remote.storage.RemoteStorageManager;
 import ru.mg.kafka.tieredstorage.minio.config.ConnectionConfig;
 import ru.mg.kafka.tieredstorage.minio.io.Fetcher;
 import ru.mg.kafka.tieredstorage.minio.io.Writer;
-import ru.mg.kafka.tieredstorage.minio.metadata.ByteEncodedMetadata;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
@@ -58,175 +51,6 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class NaiveRemoteStorageManagerTest {
-
-    @Test
-    public void testCopyLogSegmentDataWithoutTnxIndex() throws Exception {
-        final var ioWriterMock = mock(Writer.class);
-        final var ioFetcherMock = mock(Fetcher.class);
-
-        final var cfg = Map.of(
-                "minio.url", "http://0.0.0.0",
-                "minio.access.key", "access key",
-                "minio.secret.key", "secret key",
-                "minio.auto.create.bucket", false
-        );
-        when(ioFetcherMock.getConfig()).thenReturn(new ConnectionConfig(cfg));
-
-        try (var remoteStorageManager = new NaiveRemoteStorageManager(ioWriterMock, ioFetcherMock)) {
-            remoteStorageManager.configure(cfg);
-
-            when(ioWriterMock.copySegmentData(any(), any())).thenReturn(true);
-            when(ioWriterMock.copyOffsetIndex(any(), any())).thenReturn(true);
-            when(ioWriterMock.copyLeaderEpochIndex(any(), any())).thenReturn(true);
-            when(ioWriterMock.copyProducerSnapshotIndex(any(), any())).thenReturn(true);
-            when(ioWriterMock.copyTimeIndex(any(), any())).thenReturn(true);
-
-            final String topicName = "tieredTopic";
-            final int partition = 0;
-            final TopicPartition topicPartition = new TopicPartition(topicName, partition);
-
-            final Uuid topicUuid = Uuid.randomUuid();
-            final TopicIdPartition topicIdPartition = new TopicIdPartition(topicUuid, topicPartition);
-
-            final Uuid segmentUuid = Uuid.randomUuid();
-            final long segmentStartOffset = 0L;
-            final long segmentEndOffset = 1000L;
-            final long segmentMaxTimestampMs = 10000L;
-            final int brokerId = 0;
-            final long segmentEventTimestampMs = 10001L;
-            final int segmentSizeInBytes = 10;
-
-            final RemoteLogSegmentId remoteLogSegmentId = new RemoteLogSegmentId(topicIdPartition, segmentUuid);
-
-            final RemoteLogSegmentMetadata remoteLogSegmentMetadata = new RemoteLogSegmentMetadata(
-                    remoteLogSegmentId,
-                    segmentStartOffset,
-                    segmentEndOffset,
-                    segmentMaxTimestampMs,
-                    brokerId,
-                    segmentEventTimestampMs,
-                    segmentSizeInBytes,
-                    Optional.empty(),
-                    RemoteLogSegmentState.COPY_SEGMENT_STARTED,
-                    Map.of(1, 0L));
-
-            final var logSegment = Path.of("./src/test/testData/test.log").normalize().toAbsolutePath();
-            final var offsetIndex = Path.of("./src/test/testData/test.index");
-            final var timeIndex = Path.of("./src/test/testData/test.timeindex");
-            final Optional<Path> transactionalIndex = Optional.empty();
-            final var producerSnapshotIndex = Path.of("./src/test/testData/test.snapshot");
-            final var leaderEpochIndex = ByteBuffer.allocate(0);
-
-            final var logSegmentData = new LogSegmentData(
-                    logSegment,
-                    offsetIndex,
-                    timeIndex,
-                    transactionalIndex,
-                    producerSnapshotIndex,
-                    leaderEpochIndex);
-
-            final var customMetadataActual = remoteStorageManager.copyLogSegmentData(
-                    remoteLogSegmentMetadata,
-                    logSegmentData);
-
-            final var copyMetadataExpected = new ByteEncodedMetadata();
-            copyMetadataExpected.setDataNotEmpty(true);
-            copyMetadataExpected.setIndexNotEmpty(true);
-            copyMetadataExpected.setTimeIndexNotEmpty(true);
-            copyMetadataExpected.setTransactionIndexNotEmpty(false);
-            copyMetadataExpected.setProducerSnapshotIndexNotEmpty(true);
-            copyMetadataExpected.setLeaderEpochIndexNotEmpty(true);
-
-            assertTrue(customMetadataActual.isPresent());
-            final var copyMetadataActual = new ByteEncodedMetadata(customMetadataActual.get().value()[0]);
-
-            assertEquals(copyMetadataExpected.getByteValue(), copyMetadataActual.getByteValue());
-
-            verify(ioWriterMock, times(1)).copySegmentData(any(), any());
-            verify(ioWriterMock, times(1)).copyTimeIndex(any(), any());
-            verify(ioWriterMock, times(0)).copyTransactionalIndex(any(), any());
-            verify(ioWriterMock, times(1)).copyOffsetIndex(any(), any());
-            verify(ioWriterMock, times(1)).copyProducerSnapshotIndex(any(), any());
-            verify(ioWriterMock, times(1)).copyLeaderEpochIndex(any(), any());
-        }
-
-    }
-
-    @Test
-    public void testCopySegmentDataOnIOException() throws Exception {
-        final var ioWriterMock = mock(Writer.class);
-        final var ioFetcherMock = mock(Fetcher.class);
-
-        final var cfg = Map.of(
-                "minio.url", "http://0.0.0.0",
-                "minio.access.key", "access key",
-                "minio.secret.key", "secret key",
-                "minio.auto.create.bucket", false
-        );
-        when(ioFetcherMock.getConfig()).thenReturn(new ConnectionConfig(cfg));
-
-        try (var remoteStorageManager = new NaiveRemoteStorageManager(ioWriterMock, ioFetcherMock)) {
-            remoteStorageManager.configure(cfg);
-
-            when(ioWriterMock.copySegmentData(any(), any()))
-                    .thenAnswer(invocation -> {
-                        throw new RemoteStorageException("");
-                    });
-
-
-            final String topicName = "tieredTopic";
-            final int partition = 0;
-            final TopicPartition topicPartition = new TopicPartition(topicName, partition);
-
-            final Uuid topicUuid = Uuid.randomUuid();
-            final TopicIdPartition topicIdPartition = new TopicIdPartition(topicUuid, topicPartition);
-
-            final Uuid segmentUuid = Uuid.randomUuid();
-            final long segmentStartOffset = 0L;
-            final long segmentEndOffset = 1000L;
-            final long segmentMaxTimestampMs = 10000L;
-            final int brokerId = 0;
-            final long segmentEventTimestampMs = 10001L;
-            final int segmentSizeInBytes = 10;
-
-            final RemoteLogSegmentId remoteLogSegmentId = new RemoteLogSegmentId(topicIdPartition, segmentUuid);
-
-            final RemoteLogSegmentMetadata remoteLogSegmentMetadata = new RemoteLogSegmentMetadata(
-                    remoteLogSegmentId,
-                    segmentStartOffset,
-                    segmentEndOffset,
-                    segmentMaxTimestampMs,
-                    brokerId,
-                    segmentEventTimestampMs,
-                    segmentSizeInBytes,
-                    Optional.empty(),
-                    RemoteLogSegmentState.COPY_SEGMENT_STARTED,
-                    Map.of(1, 0L));
-
-            final var logSegment = Path.of("./src/test/testData/test.log").normalize().toAbsolutePath();
-            final var offsetIndex = Path.of("./src/test/testData/test.index");
-            final var timeIndex = Path.of("./src/test/testData/test.timeindex");
-            final Optional<Path> transactionalIndex = Optional.of(Path.of("./src/test/testData/test.txnindex"));
-            final var producerSnapshotIndex = Path.of("./src/test/testData/test.snapshot");
-            final var leaderEpochIndex = ByteBuffer.allocate(0);
-
-            final var logSegmentData = new LogSegmentData(
-                    logSegment,
-                    offsetIndex,
-                    timeIndex,
-                    transactionalIndex,
-                    producerSnapshotIndex,
-                    leaderEpochIndex);
-
-            assertThrows(
-                    RemoteStorageException.class,
-                    () -> remoteStorageManager.copyLogSegmentData(
-                            remoteLogSegmentMetadata,
-                            logSegmentData));
-
-            verify(ioWriterMock, times(1)).copySegmentData(any(), any());
-        }
-    }
 
     @Test
     public void testFetchSegmentFromStartPosition() throws Exception {
--- naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/NaiveRsmCopyTest.java
+++ naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/NaiveRsmCopyTest.java
@@ -19,6 +19,7 @@ package ru.mg.kafka.tieredstorage.minio;
 import java.util.Map;
 
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentMetadata;
+import org.apache.kafka.server.log.remote.storage.RemoteStorageException;
 
 import ru.mg.kafka.tieredstorage.minio.config.ConnectionConfig;
 import ru.mg.kafka.tieredstorage.minio.io.Fetcher;
@@ -30,6 +31,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -93,4 +95,78 @@ public class NaiveRsmCopyTest {
             verify(ioWriterMock, times(1)).copyLeaderEpochIndex(any(), any());
         }
     }
+
+    @Test
+    public void testCopyLogSegmentDataWithoutTnxIndex() throws Exception {
+        final var ioWriterMock = mock(Writer.class);
+        final var ioFetcherMock = mock(Fetcher.class);
+
+        when(ioFetcherMock.getConfig()).thenReturn(new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG));
+
+        try (var remoteStorageManager = new NaiveRemoteStorageManager(ioWriterMock, ioFetcherMock)) {
+            remoteStorageManager.configure(NOT_AUTO_CREATE_BUCKET_CONFIG);
+
+            when(ioWriterMock.copySegmentData(any(), any())).thenReturn(true);
+            when(ioWriterMock.copyOffsetIndex(any(), any())).thenReturn(true);
+            when(ioWriterMock.copyLeaderEpochIndex(any(), any())).thenReturn(true);
+            when(ioWriterMock.copyProducerSnapshotIndex(any(), any())).thenReturn(true);
+            when(ioWriterMock.copyTimeIndex(any(), any())).thenReturn(true);
+
+            final RemoteLogSegmentMetadata remoteLogSegmentMetadata = MetadataUtils.remoteLogSegmentMetadata();
+            final var logSegmentData = LogSegmentDataUtils.logSegmentData();
+
+            final var customMetadataActual = remoteStorageManager.copyLogSegmentData(
+                    remoteLogSegmentMetadata,
+                    logSegmentData);
+
+            final var copyMetadataExpected = new ByteEncodedMetadata();
+            copyMetadataExpected.setDataNotEmpty(true);
+            copyMetadataExpected.setIndexNotEmpty(true);
+            copyMetadataExpected.setTimeIndexNotEmpty(true);
+            copyMetadataExpected.setTransactionIndexNotEmpty(false);
+            copyMetadataExpected.setProducerSnapshotIndexNotEmpty(true);
+            copyMetadataExpected.setLeaderEpochIndexNotEmpty(true);
+
+            assertTrue(customMetadataActual.isPresent());
+            final var copyMetadataActual = new ByteEncodedMetadata(customMetadataActual.get().value()[0]);
+
+            assertEquals(copyMetadataExpected.getByteValue(), copyMetadataActual.getByteValue());
+
+            verify(ioWriterMock, times(1)).copySegmentData(any(), any());
+            verify(ioWriterMock, times(1)).copyTimeIndex(any(), any());
+            verify(ioWriterMock, times(1)).copyOffsetIndex(any(), any());
+            verify(ioWriterMock, times(1)).copyProducerSnapshotIndex(any(), any());
+            verify(ioWriterMock, times(1)).copyLeaderEpochIndex(any(), any());
+        }
+
+    }
+
+    @Test
+    public void testCopySegmentDataOnIOException() throws Exception {
+        final var ioWriterMock = mock(Writer.class);
+        final var ioFetcherMock = mock(Fetcher.class);
+
+        when(ioFetcherMock.getConfig()).thenReturn(new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG));
+
+        try (var remoteStorageManager = new NaiveRemoteStorageManager(ioWriterMock, ioFetcherMock)) {
+            remoteStorageManager.configure(NOT_AUTO_CREATE_BUCKET_CONFIG);
+
+            when(ioWriterMock.copySegmentData(any(), any()))
+                    .thenAnswer(invocation -> {
+                        throw new RemoteStorageException("");
+                    });
+
+            final RemoteLogSegmentMetadata remoteLogSegmentMetadata = MetadataUtils.remoteLogSegmentMetadata();
+            final var logSegmentData = LogSegmentDataUtils.logSegmentData();
+
+            assertThrows(
+                    RemoteStorageException.class,
+                    () -> remoteStorageManager.copyLogSegmentData(
+                            remoteLogSegmentMetadata,
+                            logSegmentData));
+
+            verify(ioWriterMock, times(1)).copySegmentData(any(), any());
+        }
+    }
+
 }
