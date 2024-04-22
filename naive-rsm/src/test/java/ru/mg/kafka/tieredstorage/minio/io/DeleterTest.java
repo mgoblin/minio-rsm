29e251d93ffef5c5fdfbfd113942e40b2329283f
Michael Golovanov <mike.golovanov@gmail.com>
2024-04-22 17:02:26 +0300

    Minio Deleter deleteSegmentObject method unit test

--- naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/io/DeleterTest.java
+++ naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/io/DeleterTest.java
@@ -16,23 +16,34 @@
 
 package ru.mg.kafka.tieredstorage.minio.io;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.List;
 import java.util.Map;
+
+import org.apache.kafka.server.log.remote.storage.RemoteStorageException;
 
 import io.minio.GetObjectResponse;
 import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InternalException;
 import io.minio.errors.ServerException;
+import io.minio.messages.ErrorResponse;
 
 import ru.mg.kafka.tieredstorage.minio.config.ConnectionConfig;
 
 import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
@@ -83,5 +94,144 @@ public class DeleterTest {
         final Deleter deleter = new Deleter(config, minioClientMock);
 
         assertFalse(deleter.objectExists("object"));
+    }
+
+    @Test
+    public void testDeleteExistingObject() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        final var response = new GetObjectResponse(
+                Headers.of(),
+                "bucket",
+                "region",
+                "objectName",
+                InputStream.nullInputStream()
+        );
+        when(minioClientMock.getObject(any())).thenReturn(response);
+
+        final Deleter deleter = new Deleter(config, minioClientMock);
+
+        deleter.deleteSegmentObject("object");
+
+        verify(minioClientMock, times(1)).getObject(any());
+        verify(minioClientMock, times(1)).removeObject(any());
+    }
+
+    @Test
+    public void testDeleteNonExistingObject() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        doThrow(ServerException.class).when(minioClientMock).getObject(any());
+
+        final Deleter deleter = new Deleter(config, minioClientMock);
+
+        deleter.deleteSegmentObject("object");
+
+        verify(minioClientMock, times(1)).getObject(any());
+        verify(minioClientMock, times(0)).removeObject(any());
+    }
+
+    @Test
+    public void testDeleteExistingObjectWithIOException() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        final var response = new GetObjectResponse(
+                Headers.of(),
+                "bucket",
+                "region",
+                "objectName",
+                InputStream.nullInputStream()
+        );
+        when(minioClientMock.getObject(any())).thenReturn(response);
+        doThrow(IOException.class).when(minioClientMock).removeObject(any());
+
+        final Deleter deleter = new Deleter(config, minioClientMock);
+
+        assertThrows(
+                RemoteStorageException.class,
+                () -> deleter.deleteSegmentObject("object"));
+
+        verify(minioClientMock, times(1)).getObject(any());
+        verify(minioClientMock, times(1)).removeObject(any());
+    }
+
+    @Test
+    public void testDeleteExistingObjectWithErrorResponseException() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        final var response = new GetObjectResponse(
+                Headers.of(),
+                "bucket",
+                "region",
+                "objectName",
+                InputStream.nullInputStream()
+        );
+
+        final var errorResponse = new ErrorResponse(
+                "404",
+                "Error",
+                "bucket",
+                "object",
+                "resource",
+                "RqUID",
+                "host");
+        final var rs = mock(Response.class);
+        when(rs.request()).thenReturn(new Request(
+                new HttpUrl(
+                        "http",
+                        "",
+                        "",
+                        "localhost",
+                        80,
+                        List.of(),
+                        List.of(),
+                        "",
+                        ""),
+                "http",
+                Headers.of(),
+                null,
+                Map.of()
+        ));
+        when(minioClientMock.getObject(any())).thenReturn(response);
+        doThrow(new ErrorResponseException(errorResponse, rs, "trace"))
+                .when(minioClientMock).removeObject(any());
+
+        final Deleter deleter = new Deleter(config, minioClientMock);
+
+        assertThrows(
+                RemoteStorageException.class,
+                () -> deleter.deleteSegmentObject("object"));
+
+        verify(minioClientMock, times(1)).getObject(any());
+        verify(minioClientMock, times(1)).removeObject(any());
+    }
+
+    @Test
+    public void testDeleteExistingObjectWithInternalException() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        final var response = new GetObjectResponse(
+                Headers.of(),
+                "bucket",
+                "region",
+                "objectName",
+                InputStream.nullInputStream()
+        );
+        when(minioClientMock.getObject(any())).thenReturn(response);
+        doThrow(InternalException.class).when(minioClientMock).removeObject(any());
+
+        final Deleter deleter = new Deleter(config, minioClientMock);
+
+        assertThrows(
+                RemoteStorageException.class,
+                () -> deleter.deleteSegmentObject("object"));
+
+        verify(minioClientMock, times(1)).getObject(any());
+        verify(minioClientMock, times(1)).removeObject(any());
     }
 }
