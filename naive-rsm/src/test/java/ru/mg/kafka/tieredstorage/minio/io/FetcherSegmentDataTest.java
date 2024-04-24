c180b778edacdbc870bf7bc00d2cba12d37f1cdf
Michael Golovanov <mike.golovanov@gmail.com>
2024-04-24 10:53:58 +0300

    Fetcher fetchLogSegmentData unit tests implemented

--- naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/io/FetcherSegmentDataTest.java
+++ naive-rsm/src/test/java/ru/mg/kafka/tieredstorage/minio/io/FetcherSegmentDataTest.java
@@ -186,4 +186,34 @@ public class FetcherSegmentDataTest {
         verify(minioClientMock, times(1)).getObject(any());
     }
 
+    @Test
+    public void testFetchLogSegmentDataFromStart() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        when(minioClientMock.getObject(any())).thenReturn(new GetObjectResponse(
+                Headers.of(), "bucket", "region", "object", InputStream.nullInputStream()
+        ));
+
+        final Fetcher fetcher = new Fetcher(config, minioClientMock);
+
+        assertNotNull(fetcher.fetchLogSegmentData("object", 0));
+        verify(minioClientMock, times(1)).getObject(any());
+    }
+
+    @Test
+    public void testFetchLogSegmentDataFromStartToEnd() throws Exception {
+        final ConnectionConfig config = new ConnectionConfig(NOT_AUTO_CREATE_BUCKET_CONFIG);
+        final var minioClientMock = mock(MinioClient.class);
+
+        when(minioClientMock.getObject(any())).thenReturn(new GetObjectResponse(
+                Headers.of(), "bucket", "region", "object", InputStream.nullInputStream()
+        ));
+
+        final Fetcher fetcher = new Fetcher(config, minioClientMock);
+
+        assertNotNull(fetcher.fetchLogSegmentData("object", 0, 500));
+        verify(minioClientMock, times(1)).getObject(any());
+    }
+
 }
