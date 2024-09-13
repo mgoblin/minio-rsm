bff01f85dc9425c49380fb61b40077944b78a065
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-13 23:17:21 +0300

    description and shell scripts for configure KRaft mode of  kafka cluster

---
+++ kafka/scripts/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export KAFKA_BASE_DIR="/home/mike/Projects/kafka/kafka_2.13-3.7.1"
+export KAFKA_DATA_DIR="/home/mike/Projects/kafka/data/tiered/kafka"
+export MINIO_DATA_DIR=/home/mike/Projects/kafka/data/tiered/minio
---
+++ kafka/scripts/prepare_kafka.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cwd=$(pwd)
+pushd "$cwd" || exit
+
+source ./env.sh
+
+rm -rf "${KAFKA_DATA_DIR:?}"/*
+
+cp "$cwd"/../config/kraft/server.properties "$KAFKA_BASE_DIR/config/kraft/server.properties"
+
+cd "$KAFKA_BASE_DIR" || exit
+UUID="$(bin/kafka-storage.sh random-uuid)"
+
+bin/kafka-storage.sh format \
+-g \
+-t "$UUID" \
+-c "$KAFKA_BASE_DIR/config/kraft/server.properties"
+
+bin/kafka-server-start.sh -daemon \
+"$KAFKA_BASE_DIR/config/kraft/server.properties"
+sleep 5s
+timeout 3m grep -q 'Kafka Server started' <(tail -f "$KAFKA_BASE_DIR/logs/server.log") || exit 1
+
+bin/kafka-server-stop.sh
+
+popd || exit
