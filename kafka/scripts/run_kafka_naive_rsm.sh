ecee2838c25fed26c0ba35664b848296ab7605ed
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-14 21:51:11 +0300

    prepare to run kafka with naive rsm config

---
+++ kafka/scripts/run_kafka_naive_rsm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cwd=$(pwd)
+pushd "$cwd" || exit
+
+source ./env.sh
+
+rm -rf "${KAFKA_BASE_DIR:?}"/logs/*
+
+cp "$cwd"/../config/kraft/naive_rsm_server.properties "$KAFKA_BASE_DIR/config/kraft/naive_rsm_server.properties"
+./copy_libs.sh
+
+./run_minio_server.sh
+
+cd "$KAFKA_BASE_DIR" || exit
+
+bin/kafka-server-start.sh -daemon \
+"$KAFKA_BASE_DIR/config/kraft/naive_rsm_server.properties"
+sleep 5s
+timeout 3m grep -q 'Kafka Server started' <(tail -f "$KAFKA_BASE_DIR/logs/server.log") || exit 1
+
+bin/kafka-server-stop.sh
+
+cd "$cwd" || exit
+
+pid=$(pgrep minio)
+
+kill "$pid"
+
+popd || exit
