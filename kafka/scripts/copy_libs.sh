25f595156057e0f6a8eda30fe654fe0f66d906d8
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-14 13:32:12 +0300

    add copy libs step to shell scripts for configure KRaft mode of  kafka cluster

---
+++ kafka/scripts/copy_libs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cwd=$(pwd)
+pushd "$cwd" || exit
+
+source ./env.sh
+
+if [ ! -f "$DISTRIBUTION_FILE" ]
+then
+  echo "Build libs"
+  cd ../../
+  gradle clean build
+fi
+
+tar -C "$KAFKA_BASE_DIR/libs" -xvf "$DISTRIBUTION_FILE" --strip-components 1
+
+popd || exit
