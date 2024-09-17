a6574bf887c087ec50a90d5aa5e0cef3e3d57a2c
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-17 07:37:05 +0300

    refactor consume_all_messages.sh bash -e option and disable popd and pushd output

--- kafka/scripts/rw_data/consume_all_messages.sh
+++ kafka/scripts/rw_data/consume_all_messages.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cwd=$(pwd)
-pushd "$cwd" || exit
+pushd "$cwd" > /dev/null
 
 source ../env.sh
-cd "$KAFKA_BASE_DIR" || exit
+cd "$KAFKA_BASE_DIR"
 
 ./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
 --topic topic1 --from-beginning --timeout-ms 10000
 
-popd || exit
+popd > /dev/null
