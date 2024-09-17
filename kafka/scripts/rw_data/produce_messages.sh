00a28ca89ada0f2e90807a9c2260934cb18dae7b
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-17 07:41:15 +0300

    refactor produce_messages.sh bash -e option and disable popd and pushd output

--- kafka/scripts/rw_data/produce_messages.sh
+++ kafka/scripts/rw_data/produce_messages.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cwd=$(pwd)
-pushd "$cwd" || exit
+pushd "$cwd" > /dev/null
 
 source ../env.sh
-cd "$KAFKA_BASE_DIR" || exit
+cd "$KAFKA_BASE_DIR"
 
 bin/kafka-producer-perf-test.sh \
    --topic topic1 --num-records=10000 --throughput -1 --record-size 1000 \
    --producer-props acks=1 batch.size=16384 bootstrap.servers=localhost:9092
 
-popd || exit
+popd > /dev/null
