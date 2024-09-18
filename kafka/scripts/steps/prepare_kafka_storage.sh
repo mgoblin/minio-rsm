ba0fd9d66cf09c78eddf9dcfce3fe2ca6a32bf40
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-18 08:43:07 +0300

    remove not used || exit 1

--- kafka/scripts/steps/prepare_kafka_storage.sh
+++ kafka/scripts/steps/prepare_kafka_storage.sh
@@ -21,9 +21,9 @@ bin/kafka-storage.sh format \
 bin/kafka-server-start.sh -daemon \
 "$KAFKA_BASE_DIR/config/kraft/server.properties"
 sleep 5s
-timeout 3m grep -q 'Kafka Server started' <(tail -f "$KAFKA_BASE_DIR/logs/server.log") || exit 1
+timeout 3m grep -q 'Kafka Server started' <(tail -f "$KAFKA_BASE_DIR/logs/server.log")
 
 bin/kafka-server-stop.sh
-timeout 30s grep -q 'Stopping SharedServer' <(tail -f "$KAFKA_BASE_DIR/logs/server.log") || exit 1
+timeout 30s grep -q 'Stopping SharedServer' <(tail -f "$KAFKA_BASE_DIR/logs/server.log")
 
 popd > /dev/null
