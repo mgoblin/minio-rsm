28cd092da89613f31f09c50fbf9a2772d3a617a0
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-18 08:44:57 +0300

    remove from prepare_kafka_storage.sh kafka broker restart

--- kafka/scripts/steps/prepare_kafka_storage.sh
+++ kafka/scripts/steps/prepare_kafka_storage.sh
@@ -18,12 +18,4 @@ bin/kafka-storage.sh format \
 -t "$UUID" \
 -c "$KAFKA_BASE_DIR/config/kraft/server.properties" > /dev/null
 
-bin/kafka-server-start.sh -daemon \
-"$KAFKA_BASE_DIR/config/kraft/server.properties"
-sleep 5s
-timeout 3m grep -q 'Kafka Server started' <(tail -f "$KAFKA_BASE_DIR/logs/server.log")
-
-bin/kafka-server-stop.sh
-timeout 30s grep -q 'Stopping SharedServer' <(tail -f "$KAFKA_BASE_DIR/logs/server.log")
-
 popd > /dev/null
