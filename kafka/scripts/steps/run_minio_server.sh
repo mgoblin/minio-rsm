f5a7c6defa01f07406fe9a058a717f62be6efff1
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-18 08:49:22 +0300

    add bash -e and disable pushd and popd output in run_minio_server.sh

--- kafka/scripts/steps/run_minio_server.sh
+++ kafka/scripts/steps/run_minio_server.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
+#!/bin/bash -e
 
 cwd=$(pwd)
-pushd "$cwd" || exit
+pushd "$cwd" > /dev/null
 
 source ../env.sh
 
-minio server --address localhost:9000 --console-address localhost:9001 "$MINIO_DATA_DIR" &
+(minio server --address localhost:9000 --console-address localhost:9001 "$MINIO_DATA_DIR" &) > /dev/null
 
-popd || exit
+popd > /dev/null
