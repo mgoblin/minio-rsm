406e89996ec940933746a2ff6aeb987a02ad3e38
Michael Golovanov <mike.golovanov@gmail.com>
2024-09-19 06:45:28 +0300

    run_kafka_naive_rsm_and_minio.sh consists only from steps now

--- kafka/scripts/run_kafka_naive_rsm_and_minio.sh
+++ kafka/scripts/run_kafka_naive_rsm_and_minio.sh
@@ -1,20 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cwd=$(pwd)
-pushd "$cwd" || exit
+pushd "$cwd" > /dev//null
 
 source ./env.sh
 
-cd ./steps || exit
+cd ./steps
+echo "Start Minio server"
 ./run_minio_server.sh
+echo "Done"
+echo "Prepare and start Kafka server"
 ./prepare_kafka_storage.sh
 ./copy_libs.sh
 ./clear_kafka_logs.sh
 ./copy_kafka_config_file.sh kraft/naive_rsm_server.properties
+echo "Prepare Kafka server complete"
 ./run_kafka_server.sh kraft/naive_rsm_server.properties
-cd .. || exit
-
-cd "$cwd"/steps || exit
+echo "Kafka server started"
 ./create_tiered_topic.sh topic1
+cd ..
 
-popd || exit
+popd > /dev/null
